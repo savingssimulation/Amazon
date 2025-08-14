680d0770fc8afa36839375f6377c43d969898bb3
savingssimulation <h0930giri@yahoo.co.jp>
2025-08-15 01:11:32 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Amazon/',
+  // publicDirを明記することで、ビルド時の挙動を安定させる
+  publicDir: 'public',
 })
